652c6e103a20e0bb174ddd4f9a141ef993719296
skandula76 <44144864+skandula76@users.noreply.github.com>
2018-10-20 21:44:01 -0400

    Update PortfolioVO.java

--- File Utilities/PSNYHack/PortfolioVO.java
+++ File Utilities/PSNYHack/PortfolioVO.java
@@ -1,9 +1,14 @@
 package PSNYHack;
 
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone; 
+
 public class PortfolioVO {
 
   public int RowNum;
-  public String DateTimeEST;
+  public Date DateTimeEST;
   public String Sector;
   public String Ticker;
   public String CompanyName;
@@ -11,18 +16,28 @@ public class PortfolioVO {
   public String Currency;
   public double Price;
 
+  private static SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm aa");
+
   public PortfolioVO(int rownum, String datetimeest, String sector, 
                           String ticker, String companyname, int unitsofholding,
                                   String currency, double price) {
+        try {
+
+            TimeZone.setDefault(TimeZone.getTimeZone("EST"));
+            formatter.setTimeZone(TimeZone.getTimeZone("EST"));
 
             this.RowNum = rownum;
-            this.DateTimeEST = datetimeest;
+            this.DateTimeEST = formatter.parse(datetimeest);
             this.Sector = sector;
             this.Ticker = ticker;
             this.CompanyName = companyname;
             this.UnitsofHolding = unitsofholding;
             this.Currency = currency;
             this.Price = price;
+
+            } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
   }
 
