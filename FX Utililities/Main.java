ff23de0311c598fef80c1c631f898c8379124635
skandula76 <44144864+skandula76@users.noreply.github.com>
2018-10-20 22:09:29 -0400

    Create Main.java

---
+++ FX Utililities/Main.java
@@ -0,0 +1,14 @@
+import PSNYHack.*;
+
+import java.util.*;
+
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+      System.out.println(FXConvert.convertFXtoUSD("EUR"));
+
+	}
+
+
+}
